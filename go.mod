f989dbdcc4203963baa1df800d603b9ed8e7ca85
Phil Killewald <reallyasi9@users.noreply.github.com>
2021-08-26 15:28:31 -0400

    Move pickem4me into an internal package

--- go.mod
+++ go.mod
@@ -1,26 +1,44 @@
 module github.com/reallyasi9/pickem4me
 
-go 1.12
+go 1.16
 
 require (
-	cloud.google.com/go v0.46.1
-	cloud.google.com/go/firestore v1.0.0
+	cloud.google.com/go v0.93.3
+	cloud.google.com/go/firestore v1.5.0
+	cloud.google.com/go/storage v1.16.0
 	github.com/360EntSecGroup-Skylar/excelize v1.4.1
-	github.com/cosiner/argv v0.0.1 // indirect
-	github.com/go-delve/delve v1.3.1 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/mattn/go-colorable v0.1.4 // indirect
-	github.com/mattn/go-isatty v0.0.10 // indirect
-	github.com/mattn/go-runewidth v0.0.4 // indirect
-	github.com/peterh/liner v1.1.0 // indirect
-	github.com/sirupsen/logrus v1.4.2 // indirect
-	github.com/spf13/cobra v0.0.5 // indirect
+	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
+	github.com/coreos/go-etcd v2.0.0+incompatible // indirect
+	github.com/cosiner/argv v0.1.0 // indirect
+	github.com/cpuguy83/go-md2man v1.0.10 // indirect
+	github.com/go-delve/delve v1.7.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/googleapis/gax-go/v2 v2.1.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
+	github.com/peterh/liner v1.2.1 // indirect
+	github.com/spf13/cobra v1.2.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	go.starlark.net v0.0.0-20191016215632-c9eda478e54e // indirect
-	golang.org/x/arch v0.0.0-20190927153633-4e8777c89be4 // indirect
-	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
-	golang.org/x/sys v0.0.0-20191010194322-b09406accb47 // indirect
-	gonum.org/v1/gonum v0.0.0-20190929233944-b20cf7805fc4
-	google.golang.org/api v0.9.0
-	gopkg.in/yaml.v2 v2.2.4 // indirect
+	github.com/ugorji/go/codec v0.0.0-20181204163529-d75b2dcb6bc8 // indirect
+	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
+	go.starlark.net v0.0.0-20210602144842-1cdb82c9e17a // indirect
+	golang.org/x/arch v0.0.0-20210727222714-28578f966459 // indirect
+	golang.org/x/exp v0.0.0-20210823210606-b36147abdb7c // indirect
+	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gonum.org/v1/gonum v0.9.3
+	google.golang.org/api v0.54.0
+	google.golang.org/genproto v0.0.0-20210825212027-de86158e7fda // indirect
+	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
+	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	honnef.co/go/tools v0.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package pickem4me
+package main
 
 import (
 	"context"
@@ -7,481 +7,50 @@ import (
 	"log"
 	"os"
 
-	"cloud.google.com/go/firestore"
-	"cloud.google.com/go/storage"
-	"gonum.org/v1/gonum/stat/distuv"
-	"google.golang.org/api/iterator"
+	"github.com/reallyasi9/pickem4me/internal/pickem4me"
 )
 
-// projectID is supplied by the environment, set automatically in GCF
-var projectID = os.Getenv("GCP_PROJECT")
+const usage = `pickem4me <picker> <slateID>
 
-// fsclient is a lazily initialized firestore client
-var fsclient *firestore.Client
+Make all your picks for you!
 
-// csclient is a Cloud Store client
-var csclient *storage.Client
+Arguments
+  - picker:  (Luke-given) name of picker.
+  - slateID: The full Firebase path to the parsed slate.
+  - model:   The full Firebase path to the model (default: use the best possible model for the type of pick)
+`
 
-// PubSubMessage is the payload of a Pub/Sub event.
-type PubSubMessage struct {
-	Data []byte `json:"data"`
-}
+func main() {
 
-// PickEmMessage tells the function what to pick and for whom.
-type PickEmMessage struct {
-	// Slate is the path to a parsed slate in Firestore
-	Slate string `json:"slate"`
-
-	// Picker is the path to a picker in Firestore
-	Picker string `json:"picker"`
-
-	// Model is a path to a model to use when picking (empty value means use the best model possible)
-	Model string `json:"model"`
-}
-
-// Team is a simple representation of a team in Firestore.
-type Team struct {
-	School string `firestore:"school"`
-	Name   string `firestore:"team"`
-}
-
-// ModelPerformance prepresents the performance of a particular model in Firestore.
-type ModelPerformance struct {
-	Model  *firestore.DocumentRef `firestore:"model"`
-	StdDev float64                `firestore:"std_dev"`
-	Bias   float64                `firestore:"bias"`
-	System string                 `firestore:"system"`
-}
-
-// ModelPrediction represents a prediction for a particular game in Firestore.
-type ModelPrediction struct {
-	Home    *firestore.DocumentRef `firestore:"home"`
-	Road    *firestore.DocumentRef `firestore:"road"`
-	Neutral bool                   `firestore:"neutral"`
-	Spread  float64                `firestore:"spread"`
-	// Ref is just a reference to the prediction itself
-	Ref *firestore.DocumentRef
-}
-
-// Picker represents a picker in Firestore.
-type Picker struct {
-	Name     string `firestore:"name"`
-	NameLuke string `firestore:"name_luke"`
-}
-
-// StreakPrediction represents a simplified version of the Beat-the-Streak output in Firestore.
-type StreakPrediction struct {
-	BestPick    []*firestore.DocumentRef `firestore:"best_pick"`
-	Probability float64                  `firestore:"probability"`
-	Spread      float64                  `firestore:"spread"`
-}
-
-// Model is a collection of performance metrics, predictions, and a distribution.
-type Model struct {
-	Performance  ModelPerformance
-	Predictions  []ModelPrediction
-	Distribution distuv.Normal
-
-	homeLookup map[string]int
-	roadLookup map[string]int
-}
-
-// Lookup prediction by home and road teams, and whether or not to swap them for the slate.
-func (m *Model) Lookup(home, road *firestore.DocumentRef) (*ModelPrediction, bool, error) {
-	if m.homeLookup == nil || m.roadLookup == nil {
-		// Make a lookup table for home and road teams
-		m.homeLookup = make(map[string]int)
-		m.roadLookup = make(map[string]int)
-		for i, model := range m.Predictions {
-			m.homeLookup[model.Home.ID] = i
-			m.roadLookup[model.Road.ID] = i
-		}
-	}
-	var hr, rr int
-	var ok, maybeSwap bool
-
-	hr, ok = m.homeLookup[home.ID]
-	// if found, everything is fine.
-	// if not, maybe Luke got home and road mixed up?
-	if !ok {
-		hr, maybeSwap = m.roadLookup[home.ID]
-		if !maybeSwap {
-			return nil, false, fmt.Errorf("home team '%s' not in predicted game", home.ID)
-		}
+	if len(os.Args) < 3 {
+		fmt.Println(usage)
+		os.Exit(0)
 	}
 
-	// if home is where it should be, road should be found as well
-	if !maybeSwap {
-		rr, ok = m.roadLookup[road.ID]
-		if !ok {
-			return nil, false, fmt.Errorf("road team '%s' not in predicted game", road.ID)
-		}
-	} else {
-		// road should be in home list
-		rr, ok = m.homeLookup[road.ID]
-		if !ok {
-			return nil, false, fmt.Errorf("road->home team '%s' not in predicted game", road.ID)
-		}
+	picker := os.Args[1]
+	slateID := os.Args[2]
+	var modelID string
+	if len(os.Args) > 3 {
+		modelID = os.Args[3]
 	}
 
-	// now check if the predicted games are the same (i.e., home is actually playing road!)
-	if hr != rr {
-		return nil, false, fmt.Errorf("home '%s' and road '%s' not playing each other", home.ID, road.ID)
+	ctx := context.Background()
+	pem := pickem4me.PickEmMessage{
+		Picker: picker,
+		Model:  modelID,
+		Slate:  slateID,
 	}
 
-	return &(m.Predictions[hr]), maybeSwap, nil
-}
-
-// PickEm consumes a Pub/Sub message.
-func PickEm(ctx context.Context, m PubSubMessage) error {
-	var pem PickEmMessage
-	err := json.Unmarshal(m.Data, &pem)
+	data, err := json.Marshal(pem)
 	if err != nil {
-		log.Printf("json.Unmarshal: %v", err)
-		return err
+		log.Fatal(err)
 	}
 
-	if fsclient == nil {
-		fsclient, err = firestore.NewClient(ctx, projectID)
-		if err != nil {
-			log.Printf("Failed making Firestore client: %v", err)
-			return err
-		}
+	m := pickem4me.PubSubMessage{
+		Data: data,
 	}
 
-	if csclient == nil {
-		csclient, err = storage.NewClient(ctx)
-		if err != nil {
-			log.Printf("Failed making Cloud Storage client: %v", err)
-			return err
-		}
+	if err := pickem4me.PickEm(ctx, m); err != nil {
+		log.Fatal(err)
 	}
-
-	// Get the slate
-	slateDoc, err := fsclient.Doc(pem.Slate).Get(ctx)
-	if err != nil {
-		log.Printf("Failed getting slate '%s': %v", pem.Slate, err)
-		return err
-	}
-	var slate Slate
-	err = slateDoc.DataTo(&slate)
-	if err != nil {
-		log.Printf("Failed parsing slate '%s': %v", pem.Slate, err)
-		return err
-	}
-	log.Printf("Got slate '%s': %v", slateDoc.Ref.ID, slate)
-
-	gameDocs, err := slateDoc.Ref.Collection("games").OrderBy("row", firestore.Asc).Documents(ctx).GetAll()
-	if err != nil {
-		log.Printf("Failed getting games from slate '%s': %v", pem.Slate, err)
-		return err
-	}
-	games := make([]SlateGame, len(gameDocs))
-	for i, doc := range gameDocs {
-		var game SlateGame
-		err = doc.DataTo(&game)
-		if err != nil {
-			log.Printf("Failed parsing game '%s': %v", doc.Ref.ID, err)
-			return err
-		}
-		log.Printf("Got game '%s': %v", doc.Ref.ID, game)
-		games[i] = game
-	}
-
-	// Get the picker
-	pickerDoc, err := fsclient.Collection("pickers").Where("name_luke", "==", pem.Picker).Limit(1).Documents(ctx).Next()
-	if err != nil {
-		log.Printf("Failed getting picker '%s': %v", pem.Picker, err)
-		return err
-	}
-	var picker Picker
-	if err := pickerDoc.DataTo(&picker); err != nil {
-		log.Printf("Failed parsing picker '%s': %v", pem.Picker, err)
-		return err
-	}
-	log.Printf("Got picker '%s': %v", pickerDoc.Ref.ID, picker)
-
-	// Figure out the model to use
-	modelPerfDocs, err := GetModels(ctx, pem.Model)
-	if err != nil {
-		log.Printf("Failed getting model '%s': %v", pem.Model, err)
-		return err
-	}
-
-	models := make(map[string]Model)
-	for gameType, doc := range modelPerfDocs {
-		var modelPerf ModelPerformance
-		if err := doc.DataTo(&modelPerf); err != nil {
-			log.Printf("Failed parsing model performance '%s': %v", pem.Model, err)
-			return err
-		}
-		log.Printf("Got model performance for '%s': %v", doc.Ref.ID, modelPerf)
-
-		predictionDocs, err := doc.Ref.Collection("predictions").Documents(ctx).GetAll()
-		if err != nil {
-			log.Printf("Failed getting predictions from model '%s': %v", pem.Model, err)
-			return err
-		}
-		preds := make([]ModelPrediction, len(predictionDocs))
-		for i, doc := range predictionDocs {
-			var prediction ModelPrediction
-			if err := doc.DataTo(&prediction); err != nil {
-				log.Printf("Failed parsing prediction '%s': %v", doc.Ref.ID, err)
-				return err
-			}
-			log.Printf("Got prediction '%s': %v", doc.Ref.ID, prediction)
-			prediction.Ref = doc.Ref
-			preds[i] = prediction
-		}
-
-		dist := distuv.Normal{
-			Mu:    modelPerf.Bias,
-			Sigma: modelPerf.StdDev,
-		}
-
-		models[gameType] = Model{Predictions: preds, Performance: modelPerf, Distribution: dist}
-	}
-
-	// Pick-a-dog
-	var pickedDog *SuperDogPick
-	var bestValue float64
-
-	// Make picks separate from slate games
-	suPicks := make([]*StraightUpPick, 0)
-	nsPicks := make([]*NoisySpreadPick, 0)
-	sdPicks := make([]*SuperDogPick, 0)
-
-	for _, game := range games {
-		gameType := "StraightUp"
-		if game.Superdog {
-			gameType = "Superdog"
-		}
-		if game.NoisySpread != 0 {
-			gameType = "NoisySpread"
-		}
-
-		model := models[gameType]
-		modelPred, swap, err := model.Lookup(game.Home, game.Road)
-		if err != nil {
-			log.Printf("Failed looking up prediction: %v", err)
-			return err
-		}
-		log.Printf("Found prediction for teams %s and %s: %v", game.Home.ID, game.Road.ID, *modelPred)
-
-		// Remember that positive spread always favors home in the predictions.
-		// It also needs to favor the overdog for calculating probability correctly.
-		spread := modelPred.Spread
-		if game.Superdog {
-			if modelPred.Road.ID == game.Underdog.ID {
-				spread *= -1
-			}
-		}
-
-		target := game.NoisySpread
-		if swap {
-			target *= -1
-		}
-		prob := model.Distribution.CDF(spread - float64(target))
-
-		// Disagreement over neutral site?
-		neutralDisagreement := game.NeutralSite != modelPred.Neutral
-
-		// Update game
-		if game.Superdog {
-			sdp := SuperDogPick{
-				Underdog:             game.Underdog,
-				Overdog:              game.Overdog,
-				Rank1:                game.Rank1,
-				Rank2:                game.Rank2,
-				Value:                game.Value,
-				NeutralSite:          game.NeutralSite != neutralDisagreement, // logic: 0,0->0, 0,1->1, 1,0->1, 1,1->0
-				NeutralDisagreement:  neutralDisagreement,
-				Swap:                 swap,
-				Pick:                 nil, // hold of on picking superdogs
-				PredictedSpread:      modelPred.Spread,
-				PredictedProbability: prob,
-				ModeledGame:          modelPred.Ref,
-				Row:                  game.Row,
-			}
-			sdPicks = append(sdPicks, &sdp)
-			ev := prob * float64(game.Value)
-			if ev > bestValue {
-				pickedDog = &sdp
-				bestValue = ev
-			}
-			continue
-		}
-
-		pick := modelPred.Home
-		if prob < 0.5 {
-			pick = modelPred.Road
-		}
-		if game.NoisySpread != 0 {
-			nsPicks = append(nsPicks, &NoisySpreadPick{
-				Home:                 modelPred.Home,
-				Road:                 modelPred.Road,
-				Rank1:                game.Rank1,
-				Rank2:                game.Rank2,
-				NoisySpread:          game.NoisySpread,
-				NeutralSite:          game.NeutralSite != neutralDisagreement, // logic: 0,0->0, 0,1->1, 1,0->1, 1,1->0
-				NeutralDisagreement:  neutralDisagreement,
-				Swap:                 swap,
-				Pick:                 pick,
-				PredictedSpread:      modelPred.Spread,
-				PredictedProbability: prob,
-				ModeledGame:          modelPred.Ref,
-				Row:                  game.Row,
-			})
-			continue
-		}
-		suPicks = append(suPicks, &StraightUpPick{
-			Home:                 modelPred.Home,
-			Road:                 modelPred.Road,
-			Rank1:                game.Rank1,
-			Rank2:                game.Rank2,
-			GOTW:                 game.GOTW,
-			NeutralSite:          game.NeutralSite != neutralDisagreement, // logic: 0,0->0, 0,1->1, 1,0->1, 1,1->0
-			NeutralDisagreement:  neutralDisagreement,
-			Swap:                 swap,
-			Pick:                 pick,
-			PredictedSpread:      modelPred.Spread,
-			PredictedProbability: prob,
-			ModeledGame:          modelPred.Ref,
-			Row:                  game.Row,
-		})
-	}
-
-	// Pick that dog!  But only if dogs are still being picked!
-	if pickedDog != nil {
-		pickedDog.Pick = pickedDog.Underdog
-	}
-
-	// Finally look up streak
-	streakPick, err := LookupStreakPick(ctx, pickerDoc.Ref, slate.Season, slate.Week)
-	if err != nil {
-		return err
-	}
-
-	// With picks in place, write to Firestore
-	picksColl := fsclient.Collection("picks")
-	picksRef := picksColl.NewDoc()
-	_, err = picksRef.Create(ctx, &Picks{
-		Season: slate.Season,
-		Week:   slate.Week,
-		Picker: pickerDoc.Ref,
-	})
-	if err != nil {
-		return fmt.Errorf("Failed to write picks to firestore: %v", err)
-	}
-
-	suColl := picksRef.Collection("straight_up")
-	nsColl := picksRef.Collection("noisy_spread")
-	sdColl := picksRef.Collection("superdog")
-	streakColl := picksRef.Collection("streak")
-	fsclient.RunTransaction(ctx, func(ctx context.Context, tx *firestore.Transaction) error {
-		for _, pick := range suPicks {
-			ref := suColl.NewDoc()
-			if err := tx.Create(ref, pick); err != nil {
-				return fmt.Errorf("Transaction failed to create StraightUpPick: %v", err)
-			}
-		}
-		for _, pick := range nsPicks {
-			ref := nsColl.NewDoc()
-			if err := tx.Create(ref, pick); err != nil {
-				return fmt.Errorf("Transaction failed to create NoisySpreadPick: %v", err)
-			}
-		}
-		for _, pick := range sdPicks {
-			ref := sdColl.NewDoc()
-			if err := tx.Create(ref, pick); err != nil {
-				return fmt.Errorf("Transaction failed to create SuperDogPick: %v", err)
-			}
-		}
-		if streakPick != nil {
-			ref := streakColl.NewDoc()
-			if err := tx.Create(ref, streakPick); err != nil {
-				return fmt.Errorf("Transaction failed to create StreakPick: %v", err)
-			}
-		}
-		return nil
-	})
-	bucket := csclient.Bucket(slate.BucketName)
-	obj := bucket.Object("picks/" + slate.File)
-	w := obj.NewWriter(ctx)
-	defer w.Close()
-	w.ObjectAttrs.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-
-	outExcel, err := newExcelFile(ctx, suPicks, nsPicks, sdPicks, streakPick)
-	if err != nil {
-		return err
-	}
-
-	outExcel.Write(w)
-
-	return nil
-}
-
-// GetModels returns the model requested by the given identifier string, or the most conservative model if an empty path is given.
-func GetModels(ctx context.Context, path string) (map[string]*firestore.DocumentSnapshot, error) {
-
-	latestTracker, err := fsclient.Collection("prediction_tracker").OrderBy("timestamp", firestore.Desc).Limit(1).Documents(ctx).Next()
-	if err != nil {
-		return nil, fmt.Errorf("Failed to get latest prediction tracker: %v", err)
-	}
-
-	modelPerfs := latestTracker.Ref.Collection("model_performance")
-	models := make(map[string]*firestore.DocumentSnapshot)
-	if path == "" {
-		log.Printf("No model requested: calculating best models at the time of pick")
-
-		// Best SUW for straight-up picks
-		greatModel, err := modelPerfs.OrderBy("suw", firestore.Desc).Limit(1).Documents(ctx).Next()
-		if err != nil {
-			return nil, fmt.Errorf("Failed to get great model: %v", err)
-		}
-		models["StraightUp"] = greatModel
-
-		// Lowest MAE for noisy spreads and superdogs
-		bestModel, err := modelPerfs.OrderBy("mae", firestore.Asc).Limit(1).Documents(ctx).Next()
-		if err != nil {
-			return nil, fmt.Errorf("Failed to get best model: %v", err)
-		}
-		models["NoisySpread"] = bestModel
-		models["Superdog"] = bestModel
-
-		return models, nil
-	}
-
-	// Just give me whatever you ask for
-	modelRef := fsclient.Doc(path)
-	model, err := modelPerfs.Where("model", "==", modelRef).Limit(1).Documents(ctx).Next()
-	if err != nil {
-		return nil, fmt.Errorf("Failed to get model at path '%s': %v", path, err)
-	}
-	models["StraightUp"] = model
-	models["NoisySpread"] = model
-	models["Superdog"] = model
-
-	return models, nil
-}
-
-// LookupStreakPick looks up the streak pick for a picker in Firestore
-func LookupStreakPick(ctx context.Context, picker, season *firestore.DocumentRef, week int) (*StreakPick, error) {
-	// NOTE: the streak prediction is performed for the previous week.
-	streakPredictionDoc, err := fsclient.Collection("streak_predictions").Where("picker", "==", picker).Where("season", "==", season).Where("week", "==", week-1).Limit(1).Documents(ctx).Next()
-	if err == iterator.Done {
-		// no streak yet, but that's okay!
-		return nil, nil
-	}
-	if err != nil {
-		return nil, fmt.Errorf("Failed getting streak prediction for picker '%s', season '%s', week %d: %v", picker.ID, season.ID, week, err)
-	}
-	var streakPrediction StreakPrediction
-	if err := streakPredictionDoc.DataTo(&streakPrediction); err != nil {
-		return nil, fmt.Errorf("Failed parsing streak prediction for picker '%s', season '%s', week %d: %v", picker.ID, season.ID, week, err)
-	}
-	streakPick := &StreakPick{Picks: streakPrediction.BestPick,
-		PredictedProbability: streakPrediction.Probability,
-		PredictedSpread:      streakPrediction.Spread}
-	return streakPick, nil
 }
